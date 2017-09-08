d9654c1172ff6773dd89c7af69f58cb681b56181
Kevin Risden <krisden@apache.org>
2017-09-08 10:38:29 -0500

    Cleanup testing

--- run_test.sh
+++ run_test.sh
@@ -3,5 +3,9 @@
 set -e
 set -u
 
-./add_hdfs_repository.sh
+echo "Add HDFS Repository"
+curl -i -H 'Content-type: application/json' -H 'Accept: application/json' -XPUT "http://localhost:$(docker-compose port elasticsearch 9200 | cut -d':' -f 2)/_snapshot/my_hdfs_repository" -d '{"type": "hdfs", "settings": {"uri": "hdfs://namenode.example.com/", "path": "/tmp/my_hdfs_repository", "security.principal": "hdfs/elasticsearch.example.com@EXAMPLE.COM"}}'
+
+echo "Check HDFS repository"
+curl -i -H 'Content-type: application/json' -H 'Accept: application/json' "http://localhost:$(docker-compose port elasticsearch 9200 | cut -d':' -f 2)/_snapshot/my_hdfs_repository/_all?pretty"
 
