0366fc5b391012b5bca895f7904d31c6b85e244c
Kevin Risden <krisden@apache.org>
2017-09-08 10:42:47 -0500

    Improve debug output

--- run_test.sh
+++ run_test.sh
@@ -5,7 +5,9 @@ set -u
 
 echo "Add HDFS Repository"
 curl --fail -i -H 'Content-type: application/json' -H 'Accept: application/json' -XPUT "http://localhost:$(docker-compose port elasticsearch 9200 | cut -d':' -f 2)/_snapshot/my_hdfs_repository" -d '{"type": "hdfs", "settings": {"uri": "hdfs://namenode.example.com/", "path": "/tmp/my_hdfs_repository", "security.principal": "hdfs/elasticsearch.example.com@EXAMPLE.COM"}}'
-
+echo
+echo
 echo "Check HDFS repository"
 curl --fail -i -H 'Content-type: application/json' -H 'Accept: application/json' "http://localhost:$(docker-compose port elasticsearch 9200 | cut -d':' -f 2)/_snapshot/my_hdfs_repository/_all?pretty"
+echo
 
